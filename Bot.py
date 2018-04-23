6f36a8fc4a5982249c52f22d8230a31d9da364f8
guile.hm <guile.hm@hotmail.com>
2018-04-23 11:48:42 -0300

    few changes

--- Bot.py
+++ Bot.py
@@ -5,7 +5,12 @@ from chatterbot import ChatBot
 bot = ChatBot('GuilehmBOT')
 
 conv = ['oi', 'olá', 'olá, bom dia','bom dia', 'bom dia, como vai?',
-        'eu vou bem, e você?','comigo tudo ótimo',
+        'eu vou bem, e você?','comigo tudo ótimo','você estuda?',
+        'sim, estudo programação em Python', 'eu estudo JavaScript',
+        'como você chama?','me chamo GuilehmBOT', 'como você foi criado?',
+        'eu fui criado em Python', 'quais são seus hobbies?',
+        'cubo mágico e programação','que legal!', 'sim, eu gosto muito',
+        'você trabalha?', 'sim, sou desenvolvedor',
         ]
 
 bot.set_trainer(ListTrainer)
